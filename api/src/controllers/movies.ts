d06e90d08a890bcd5066f46af63266836e4e70d2
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2022-01-07 16:06:41 +0530

    feat: finish all API endpoints

--- api/src/controllers/movies.ts
+++ api/src/controllers/movies.ts
@@ -1,16 +1,61 @@
-import { Context } from "../deps.ts";
+import { Context, helpers } from "../deps.ts";
 import { movies } from "../db/mod.ts"
+import { errorBuilder, successBuilder } from "../helpers/mod.ts";
 
 export const randomMovies = async (ctx: Context) => {
 
-    const random = await movies.find()
-                .limit(10)
-                .skip(Math.floor(Math.random() * await movies.countDocuments()))
-                .toArray();
+    const query = helpers.getQuery(ctx);
 
-    return ctx.response.body = {
-        success: true,
-        movies: random
+    let list = await movies.find()
+        .sort({ upvotes: -1 })
+        .limit(10)
+        .toArray();
+
+    if(query.genre) list = list.filter(movie => movie.genre === query.genre);
+    return ctx.response.body = successBuilder(list);
+
+}
+
+export const fetchMovie = async (ctx: MovieContext) => {
+
+    const id = ctx.params.id;
+    if(!id) return ctx.response.body = errorBuilder("Id is required");
+
+    const movie = await movies.find({ id }).limit(1).next();
+    if(!movie) return ctx.response.body = errorBuilder("Movie not found");
+
+    return ctx.response.body = successBuilder(movie);
+
+}
+
+export const upvote = async (ctx: MovieContext) => {
+
+    const id = ctx.params.id;
+    if(!id) return ctx.response.body = errorBuilder("Id is required");
+
+    const update = await movies.updateOne({ id }, { $inc: { upvotes: 1 } });
+    if(!update.matchedCount || !update.modifiedCount) return ctx.response.body = errorBuilder("Movie not found");
+
+    const movie = await movies.find({ id }).limit(1).next();
+    return ctx.response.body = successBuilder(movie);
+
+}
+
+export const downvote = async (ctx: MovieContext) => {
+
+    const id = ctx.params.id;
+    if(!id) return ctx.response.body = errorBuilder("Id is required");
+
+    const update = await movies.updateOne({ id }, { $inc: { downVotes: 1 } });
+    if(!update.matchedCount || !update.modifiedCount) return ctx.response.body = errorBuilder("Movie not found");
+
+    const movie = await movies.find({ id }).limit(1).next();
+    return ctx.response.body = successBuilder(movie);
+
+}
+
+interface MovieContext extends Context {
+    params: {
+        id: string
     }
-
 }
--- api/src/routes/mod.ts
+++ api/src/routes/mod.ts
@@ -1,9 +1,13 @@
 import { Router } from "../deps.ts";
-import { randomMovies, searchMovies } from "../controllers/mod.ts";
+import { randomMovies, searchMovies, fetchMovie, upvote, downvote } from "../controllers/mod.ts";
 
 const router = new Router();
 
-router.get('/movies', randomMovies);
-router.get('/search', searchMovies);
+router
+    .get('/movies', randomMovies)
+    .get('/movies/:id', fetchMovie)
+    .get('/movies/:id/upvote', upvote)
+    .get('/movies/:id/downvote', downvote)
+    .get('/search', searchMovies);
 
 export default router;
